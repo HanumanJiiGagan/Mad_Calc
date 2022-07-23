7f7f22314a6bcfaed5850a488d5daa5414a20805
HanumanJiiGagan <109843754+HanumanJiiGagan@users.noreply.github.com>
2022-07-23 19:53:39 +0530

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,16 @@
+a = int(input(" To Add Enter first number:"))
+b = int(input(" To Add Enter second number:"))
+sum = a+b
+print(sum)
+a = int(input(" To Subtract Enter first number:"))
+b = int(input(" To Subtract Enter second number:"))
+sum = a-b
+print("",sum)
+a = int(input(" To Multiply Enter first number:"))
+b = int(input(" To Multiply Enter second number:"))
+sum = a*b
+print(sum)
+a = int(input(" To Divide Enter first number:"))
+b = int(input(" To Divide Enter second number:"))
+sum = a/b
+print(sum)
